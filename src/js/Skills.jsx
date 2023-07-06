257d84a5b6471e2d39b8718277074b4cab7e8c07
kgalip <kemalgalip22@hotmail.com>
2023-07-07 02:50:14 +0300

    Added Jest and changed skills section animation method

--- src/js/Skills.jsx
+++ src/js/Skills.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import cssLogo from "../img/svg/css-logo.svg";
 import sassLogo from "../img/svg/sass-logo.svg";
-import javascriptLogo from "../img/svg/javascript-logo.svg";
 import typescriptLogo from "../img/svg/typescript-logo.svg";
 import reactLogo from "../img/svg/react-logo.svg";
 import tailwindLogo from "../img/svg/tailwind-logo.svg";
+import jestLogo from "../img/svg/jest-logo.svg";
 import gitLogo from "../img/svg/git-logo.svg";
 
 export default function Skills() {
@@ -35,71 +34,23 @@ export default function Skills() {
                 <h2>What I Do</h2>
                 <div className="skills-logos-parent">
                     <div className="skills-logos-div">
-                        <figure
-                            style={{ "--order": 1 }}
-                            onMouseEnter={() => {
-                                setCurrentLogo(1);
-                            }}
-                            onMouseLeave={() => {
-                                setCurrentLogo(null);
-                            }}
-                        >
-                            <img className={currentLogo === null ? "" : currentLogo === 1 ? "hover" : "not-hover"} src={sassLogo} title="SASS/SCSS" alt="SASS/SCSS" width="60" height="60" style={{ "--order": 1 }} />
+                        <figure style={{ "--order": 1 }}>
+                            <img src={sassLogo} title="SASS/SCSS" alt="SASS/SCSS" width="60" height="60" style={{ "--order": 1 }} />
                         </figure>
-                        <figure
-                            style={{ "--order": 2 }}
-                            onMouseEnter={() => {
-                                setCurrentLogo(2);
-                            }}
-                            onMouseLeave={() => {
-                                setCurrentLogo(null);
-                            }}
-                        >
-                            <img className={currentLogo === null ? "" : currentLogo === 2 ? "hover" : "not-hover"} src={javascriptLogo} title="JavaScript" alt="JavaScript" width="60" height="60" style={{ "--order": 2 }} />
+                        <figure style={{ "--order": 2 }}>
+                            <img src={typescriptLogo} title="TypeScript" alt="TypeScript" width="60" height="60" style={{ "--order": 2 }} />
                         </figure>
-                        <figure
-                            style={{ "--order": 3 }}
-                            onMouseEnter={() => {
-                                setCurrentLogo(3);
-                            }}
-                            onMouseLeave={() => {
-                                setCurrentLogo(null);
-                            }}
-                        >
-                            <img className={currentLogo === null ? "" : currentLogo === 3 ? "hover" : "not-hover"} src={typescriptLogo} title="TypeScript" alt="TypeScript" width="60" height="60" style={{ "--order": 3 }} />
+                        <figure style={{ "--order": 3 }}>
+                            <img src={reactLogo} title="ReactJS" alt="ReactJS" width="60" height="60" style={{ "--order": 3 }} />
                         </figure>
-                        <figure
-                            style={{ "--order": 4 }}
-                            onMouseEnter={() => {
-                                setCurrentLogo(4);
-                            }}
-                            onMouseLeave={() => {
-                                setCurrentLogo(null);
-                            }}
-                        >
-                            <img className={currentLogo === null ? "" : currentLogo === 4 ? "hover" : "not-hover"} src={reactLogo} title="ReactJS" alt="ReactJS" width="60" height="60" style={{ "--order": 4 }} />
+                        <figure style={{ "--order": 4 }}>
+                            <img src={tailwindLogo} title="TailwindCSS" alt="TailwindCSS" width="60" height="60" style={{ "--order": 4 }} />
                         </figure>
-                        <figure
-                            style={{ "--order": 5 }}
-                            onMouseEnter={() => {
-                                setCurrentLogo(5);
-                            }}
-                            onMouseLeave={() => {
-                                setCurrentLogo(null);
-                            }}
-                        >
-                            <img className={currentLogo === null ? "" : currentLogo === 5 ? "hover" : "not-hover"} src={tailwindLogo} title="TailwindCSS" alt="TailwindCSS" width="60" height="60" style={{ "--order": 5 }} />
+                        <figure style={{ "--order": 5 }}>
+                            <img src={jestLogo} title="Jest" alt="Jest" width="60" height="60" style={{ "--order": 5 }} />
                         </figure>
-                        <figure
-                            style={{ "--order": 6 }}
-                            onMouseEnter={() => {
-                                setCurrentLogo(6);
-                            }}
-                            onMouseLeave={() => {
-                                setCurrentLogo(null);
-                            }}
-                        >
-                            <img className={currentLogo === null ? "" : currentLogo === 6 ? "hover" : "not-hover"} src={gitLogo} title="Git" alt="Git" width="60" height="60" style={{ "--order": 6 }} />
+                        <figure style={{ "--order": 6 }}>
+                            <img src={gitLogo} title="Git" alt="Git" width="60" height="60" style={{ "--order": 6 }} />
                         </figure>
                     </div>
                 </div>
@@ -126,6 +77,9 @@ export default function Skills() {
                         </li>
                         <li>
                             <span>TailwindCSS</span>
+                        </li>
+                        <li>
+                            <span>Jest</span>
                         </li>
                         <li>
                             <span>Git and Github</span>
