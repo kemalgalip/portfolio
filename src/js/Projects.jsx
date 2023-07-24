fbfb612e012af3d03b54fbc811871e8a679ae35d
kgalip <kemalgalip22@hotmail.com>
2023-07-24 16:24:22 +0300

    Added Next.js and npm to skills, Added NarutoPedia to projects, Re-ordered projects, Changed project links to Vercel

--- src/js/Projects.jsx
+++ src/js/Projects.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import chatONSS from "../img/ss/chaton-screenshot.png";
-import twitterCloneSS from "../img/ss/twitterclone-screenshot.png";
-import skyCastSS from "../img/ss/skycast-screenshot.png";
+
+import narutopediaSS from "../img/ss/narutopedia-screenshot.png";
 import hairCoutureSS from "../img/ss/hair-couture-screenshot.png";
+import skyCastSS from "../img/ss/skycast-screenshot.png";
+import twitterCloneSS from "../img/ss/twitterclone-screenshot.png";
+import chatONSS from "../img/ss/chaton-screenshot.png";
+
 import cssLogo from "../img/svg/css-logo.svg";
 import sassLogo from "../img/svg/sass-logo.svg";
 import javascriptLogo from "../img/svg/javascript-logo.svg";
 import typescriptLogo from "../img/svg/typescript-logo.svg";
 import reactLogo from "../img/svg/react-logo.svg";
+import nextLogo from "../img/svg/next-logo.svg";
 import tailwindLogo from "../img/svg/tailwind-logo.svg";
 
 export default function Projects() {
@@ -16,6 +20,7 @@ export default function Projects() {
     const projectTwoRef = React.useRef(null);
     const projectThreeRef = React.useRef(null);
     const projectFourRef = React.useRef(null);
+    const projectFiveRef = React.useRef(null);
 
     React.useEffect(() => {
         if (projectsRef.current) {
@@ -102,29 +107,79 @@ export default function Projects() {
         }
     }, [projectFourRef.current]);
 
+    React.useEffect(() => {
+        if (projectFiveRef.current) {
+            const observer = new IntersectionObserver(
+                ([entry]) => {
+                    if (entry.isIntersecting) {
+                        projectFiveRef.current.setAttribute("data-intersect", "");
+                    }
+                },
+                { rootMargin: "0px 0px -175px 0px" }
+            );
+
+            observer.observe(projectFiveRef.current);
+
+            return () => observer.unobserve(projectFiveRef.current);
+        }
+    }, [projectFiveRef.current]);
+
     return (
         <section className="projects-section">
             <article ref={projectsRef} className="projects-article">
                 <h2>What I Did</h2>
-                <p>Here are some of my latest projects. In these projects, different parts of the web development principles and technologies are used. Details are explained in descriptions.</p>
+                <p>
+                    Here are some of my latest projects. In these projects, different parts of the
+                    web development principles and technologies are used. Details are explained in
+                    descriptions.
+                </p>
                 <div>
                     <div ref={projectOneRef} className="project-card">
-                        <a href="https://kgalip-chaton.netlify.app" target="_blank">
-                            <img src={chatONSS} width="3840" height="10668" />
+                        <a href="https://kemalgalip-narutopedia.vercel.app" target="_blank">
+                            <img src={narutopediaSS} width="960" height="1474" />
                         </a>
                         <div>
-                            <h3>ChatON - Chat App Introduction</h3>
-                            <p>CSS focused, animation rich project. Visual satisfaction was the first priority. I used 2 ready libraries to dive in to npm libraries and build something more quickly ("Features" and "FAQs" sections).</p>
+                            <h3>NarutoPedia - Naurto Universe</h3>
+                            <p>
+                                Informative server side application about the popular anime/manga
+                                series Naruto. Dynamic routing and pagination used. Built with
+                                Next.js version 13.
+                            </p>
                             <div>
                                 <div className="project-card-img-div">
-                                    <img src={cssLogo} title="CSS" alt="CSS" width="45" height="45" />
-                                    <img src={reactLogo} title="React" alt="React" width="45" height="45" />
+                                    <img
+                                        src={sassLogo}
+                                        title="SASS/SCSS"
+                                        alt="SASS/SCSS"
+                                        width="45"
+                                        height="45"
+                                    />
+                                    <img
+                                        src={typescriptLogo}
+                                        title="TypeScript"
+                                        alt="TypeScript"
+                                        width="45"
+                                        height="45"
+                                    />
+                                    <img
+                                        src={nextLogo}
+                                        title="Next.js"
+                                        alt="Next.js"
+                                        width="45"
+                                        height="45"
+                                    />
                                 </div>
                                 <div className="project-card-link-div">
-                                    <a href="https://github.com/kgalip/chat-on" target="_blank">
+                                    <a
+                                        href="https://github.com/kgalip/hair-couture"
+                                        target="_blank"
+                                    >
                                         Source
                                     </a>
-                                    <a href="https://kgalip-chaton.netlify.app" target="_blank">
+                                    <a
+                                        href="https://kemalgalip-narutopedia.vercel.app"
+                                        target="_blank"
+                                    >
                                         Live
                                     </a>
                                 </div>
@@ -132,23 +187,51 @@ export default function Projects() {
                         </div>
                     </div>
                     <div ref={projectTwoRef} className="project-card">
-                        <a href="https://kgalip-twitterclone.netlify.app" target="_blank">
-                            <img src={twitterCloneSS} width="3840" height="3726" />
+                        <a href="https://kemalgalip-haircouture.vercel.app" target="_blank">
+                            <img src={hairCoutureSS} width="960" height="2533" />
                         </a>
                         <div>
-                            <h3>Twitter - Clone Website</h3>
-                            <p>The website clone of Twitter, built using the TailwindCSS framework, automatically presents tweet layouts tailored to various data types such as images, replies, and quoted tweets.</p>
+                            <h3>Hair Couture - Hair Salon Website</h3>
+                            <p>
+                                General purpose hair salon website. Appointment and contact form
+                                submits are handled with RegEx. It is also built with React Router
+                                DOM, providing multi-page-like experience.
+                            </p>
                             <div>
                                 <div className="project-card-img-div">
-                                    <img src={javascriptLogo} title="JavaScript" alt="JavaScript" width="45" height="45" />
-                                    <img src={reactLogo} title="React" alt="React" width="45" height="45" />
-                                    <img src={tailwindLogo} title="TailwindCSS" alt="TailwindCSS" width="45" height="45" />
+                                    <img
+                                        src={sassLogo}
+                                        title="SASS/SCSS"
+                                        alt="SASS/SCSS"
+                                        width="45"
+                                        height="45"
+                                    />
+                                    <img
+                                        src={javascriptLogo}
+                                        title="JavaScript"
+                                        alt="JavaScript"
+                                        width="45"
+                                        height="45"
+                                    />
+                                    <img
+                                        src={reactLogo}
+                                        title="ReactJS"
+                                        alt="ReactJS"
+                                        width="45"
+                                        height="45"
+                                    />
                                 </div>
                                 <div className="project-card-link-div">
-                                    <a href="https://github.com/kgalip/twitter-clone" target="_blank">
+                                    <a
+                                        href="https://github.com/kgalip/hair-couture"
+                                        target="_blank"
+                                    >
                                         Source
                                     </a>
-                                    <a href="https://kgalip-twitterclone.netlify.app" target="_blank">
+                                    <a
+                                        href="https://kemalgalip-haircouture.vercel.app"
+                                        target="_blank"
+                                    >
                                         Live
                                     </a>
                                 </div>
@@ -156,23 +239,45 @@ export default function Projects() {
                         </div>
                     </div>
                     <div ref={projectThreeRef} className="project-card">
-                        <a href="https://kgalip-skycast.netlify.app" target="_blank">
-                            <img src={skyCastSS} width="3840" height="3726" />
+                        <a href="https://kemalgalip-skycast.vercel.app" target="_blank">
+                            <img src={skyCastSS} width="960" height="932" />
                         </a>
                         <div>
                             <h3>SkyCast - Weather Forecast</h3>
-                            <p>Contains some advanced JavaScript syntax (fetching different datas from the same API at the same time, getting and setting data from/to local storage, dark-light mode etc.).</p>
+                            <p>
+                                Contains some advanced JavaScript syntax (fetching different datas
+                                from the same API at the same time, getting and setting data from/to
+                                local storage, dark-light mode etc.).
+                            </p>
                             <div>
                                 <div className="project-card-img-div">
-                                    <img src={cssLogo} title="CSS" alt="CSS" width="45" height="45" />
-                                    <img src={typescriptLogo} title="TypeScript" alt="TypeScript" width="45" height="45" />
-                                    <img src={reactLogo} title="React" alt="React" width="45" height="45" />
+                                    <img
+                                        src={cssLogo}
+                                        title="CSS"
+                                        alt="CSS"
+                                        width="45"
+                                        height="45"
+                                    />
+                                    <img
+                                        src={typescriptLogo}
+                                        title="TypeScript"
+                                        alt="TypeScript"
+                                        width="45"
+                                        height="45"
+                                    />
+                                    <img
+                                        src={reactLogo}
+                                        title="ReactJS"
+                                        alt="ReactJS"
+                                        width="45"
+                                        height="45"
+                                    />
                                 </div>
                                 <div className="project-card-link-div">
                                     <a href="https://github.com/kgalip/sky-cast" target="_blank">
                                         Source
                                     </a>
-                                    <a href="https://kgalip-skycast.netlify.app" target="_blank">
+                                    <a href="https://kemalgalip-skycast.vercel.app" target="_blank">
                                         Live
                                     </a>
                                 </div>
@@ -180,23 +285,90 @@ export default function Projects() {
                         </div>
                     </div>
                     <div ref={projectFourRef} className="project-card">
-                        <a href="https://kgalip-haircouture.netlify.app" target="_blank">
-                            <img src={hairCoutureSS} width="3840" height="10024" />
+                        <a href="https://kemalgalip-twitterclone.vercel.app" target="_blank">
+                            <img src={twitterCloneSS} width="960" height="901" />
                         </a>
                         <div>
-                            <h3>Hair Couture - Hair Salon Website</h3>
-                            <p>General purpose hair salon website. Appointment and contact form submits are handled with RegEx. It is also built with React Router DOM, providing multi-page-like experience.</p>
+                            <h3>Twitter - Clone Website</h3>
+                            <p>
+                                The website clone of Twitter, built using the TailwindCSS framework,
+                                automatically presents tweet layouts tailored to various data types
+                                such as images, replies, and quoted tweets.
+                            </p>
                             <div>
                                 <div className="project-card-img-div">
-                                    <img src={sassLogo} title="SCSS" alt="SCSS" width="45" height="45" />
-                                    <img src={javascriptLogo} title="JavaScript" alt="JavaScript" width="45" height="45" />
-                                    <img src={reactLogo} title="React" alt="React" width="45" height="45" />
+                                    <img
+                                        src={javascriptLogo}
+                                        title="JavaScript"
+                                        alt="JavaScript"
+                                        width="45"
+                                        height="45"
+                                    />
+                                    <img
+                                        src={reactLogo}
+                                        title="ReactJS"
+                                        alt="ReactJS"
+                                        width="45"
+                                        height="45"
+                                    />
+                                    <img
+                                        src={tailwindLogo}
+                                        title="TailwindCSS"
+                                        alt="TailwindCSS"
+                                        width="45"
+                                        height="45"
+                                    />
                                 </div>
                                 <div className="project-card-link-div">
-                                    <a href="https://github.com/kgalip/hair-couture" target="_blank">
+                                    <a
+                                        href="https://github.com/kgalip/twitter-clone"
+                                        target="_blank"
+                                    >
                                         Source
                                     </a>
-                                    <a href="https://kgalip-haircouture.netlify.app" target="_blank">
+                                    <a
+                                        href="https://kemalgalip-twitterclone.vercel.app"
+                                        target="_blank"
+                                    >
+                                        Live
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div ref={projectFiveRef} className="project-card">
+                        <a href="https://kemalgalip-chaton.vercel.app" target="_blank">
+                            <img src={chatONSS} width="960" height="2667" />
+                        </a>
+                        <div>
+                            <h3>ChatON - Chat App Introduction</h3>
+                            <p>
+                                CSS focused, animation rich project. Visual satisfaction was the
+                                first priority. I used 2 ready libraries to dive in to npm libraries
+                                and build something more quickly ("Features" and "FAQs" sections).
+                            </p>
+                            <div>
+                                <div className="project-card-img-div">
+                                    <img
+                                        src={cssLogo}
+                                        title="CSS"
+                                        alt="CSS"
+                                        width="45"
+                                        height="45"
+                                    />
+                                    <img
+                                        src={reactLogo}
+                                        title="ReactJS"
+                                        alt="ReactJS"
+                                        width="45"
+                                        height="45"
+                                    />
+                                </div>
+                                <div className="project-card-link-div">
+                                    <a href="https://github.com/kgalip/chat-on" target="_blank">
+                                        Source
+                                    </a>
+                                    <a href="https://kemalgalip-chaton.vercel.app" target="_blank">
                                         Live
                                     </a>
                                 </div>
--- src/js/Skills.jsx
+++ src/js/Skills.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import sassLogo from "../img/svg/sass-logo.svg";
 import typescriptLogo from "../img/svg/typescript-logo.svg";
 import reactLogo from "../img/svg/react-logo.svg";
+import nextLogo from "../img/svg/next-logo.svg";
 import tailwindLogo from "../img/svg/tailwind-logo.svg";
 import jestLogo from "../img/svg/jest-logo.svg";
-import gitLogo from "../img/svg/git-logo.svg";
 
 export default function Skills() {
     const skillsRef = React.useRef(null);
@@ -35,22 +35,64 @@ export default function Skills() {
                 <div className="skills-logos-parent">
                     <div className="skills-logos-div">
                         <figure style={{ "--order": 1 }}>
-                            <img src={sassLogo} title="SASS/SCSS" alt="SASS/SCSS" width="60" height="60" style={{ "--order": 1 }} />
+                            <img
+                                src={sassLogo}
+                                title="SASS/SCSS"
+                                alt="SASS/SCSS"
+                                width="60"
+                                height="60"
+                                style={{ "--order": 1 }}
+                            />
                         </figure>
                         <figure style={{ "--order": 2 }}>
-                            <img src={typescriptLogo} title="TypeScript" alt="TypeScript" width="60" height="60" style={{ "--order": 2 }} />
+                            <img
+                                src={typescriptLogo}
+                                title="TypeScript"
+                                alt="TypeScript"
+                                width="60"
+                                height="60"
+                                style={{ "--order": 2 }}
+                            />
                         </figure>
                         <figure style={{ "--order": 3 }}>
-                            <img src={reactLogo} title="ReactJS" alt="ReactJS" width="60" height="60" style={{ "--order": 3 }} />
+                            <img
+                                src={reactLogo}
+                                title="ReactJS"
+                                alt="ReactJS"
+                                width="60"
+                                height="60"
+                                style={{ "--order": 3 }}
+                            />
                         </figure>
                         <figure style={{ "--order": 4 }}>
-                            <img src={tailwindLogo} title="TailwindCSS" alt="TailwindCSS" width="60" height="60" style={{ "--order": 4 }} />
+                            <img
+                                src={nextLogo}
+                                title="Next.js"
+                                alt="Next.js"
+                                width="60"
+                                height="60"
+                                style={{ "--order": 4 }}
+                            />
                         </figure>
                         <figure style={{ "--order": 5 }}>
-                            <img src={jestLogo} title="Jest" alt="Jest" width="60" height="60" style={{ "--order": 5 }} />
+                            <img
+                                src={tailwindLogo}
+                                title="TailwindCSS"
+                                alt="TailwindCSS"
+                                width="60"
+                                height="60"
+                                style={{ "--order": 5 }}
+                            />
                         </figure>
                         <figure style={{ "--order": 6 }}>
-                            <img src={gitLogo} title="Git" alt="Git" width="60" height="60" style={{ "--order": 6 }} />
+                            <img
+                                src={jestLogo}
+                                title="Jest"
+                                alt="Jest"
+                                width="60"
+                                height="60"
+                                style={{ "--order": 6 }}
+                            />
                         </figure>
                     </div>
                 </div>
@@ -76,17 +118,27 @@ export default function Skills() {
                             <span>ReactJS</span>
                         </li>
                         <li>
+                            <span>Next.js</span>
+                        </li>
+                        <li>
                             <span>TailwindCSS</span>
                         </li>
                         <li>
                             <span>Jest</span>
                         </li>
                         <li>
+                            <span>npm</span>
+                        </li>
+                        <li>
                             <span>Git and Github</span>
                         </li>
                     </ul>
                 </div>
-                <p>In addition to my skills, I possess a strong aptitude for learning new tools. I am a quick learner, capable of adapting to project requirements and acquiring proficiency in additional technologies as needed.</p>
+                <p>
+                    In addition to my skills, I possess a strong aptitude for learning new tools. I
+                    am a quick learner, capable of adapting to project requirements and acquiring
+                    proficiency in additional technologies as needed.
+                </p>
             </article>
         </section>
     );
